af5ba4a69c2be0195450957f0a07a1f237a6dbe7
Ales Justin <ales.justin@gmail.com>
2023-05-17 16:58:56 +0200

    Mimic reproducer from issue #33024

--- integration-tests/grpc-exceptions/src/main/java/com/example/grpc/exc/LegacyHelloGrpcService.java
+++ integration-tests/grpc-exceptions/src/main/java/com/example/grpc/exc/LegacyHelloGrpcService.java
@@ -1,7 +1,12 @@
 package com.example.grpc.exc;
 
-import io.grpc.Status;
+import com.google.protobuf.Any;
+import com.google.rpc.ErrorInfo;
+import com.google.rpc.Status;
+
 import io.grpc.StatusException;
+import io.grpc.StatusRuntimeException;
+import io.grpc.protobuf.StatusProto;
 import io.grpc.stub.StreamObserver;
 import io.quarkus.grpc.GrpcService;
 
@@ -10,7 +15,23 @@ public class LegacyHelloGrpcService extends LegacyHelloGrpcGrpc.LegacyHelloGrpcI
     @Override
     public void legacySayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
         // it NEEDS to be plain StatusException, and NOT StatusRuntimeException ?!
-        final StatusException t = new StatusException(Status.INVALID_ARGUMENT);
+        final StatusException t = new StatusException(io.grpc.Status.INVALID_ARGUMENT);
+        responseObserver.onError(t);
+    }
+
+    @Override
+    public void legacySayHelloRuntime(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        ErrorInfo errorInfo = ErrorInfo.newBuilder()
+                .setDomain("org.acme.test")
+                .setReason("stub-error")
+                .build();
+
+        final StatusRuntimeException t = StatusProto.toStatusRuntimeException(
+                Status.newBuilder()
+                        .setCode(io.grpc.Status.INVALID_ARGUMENT.getCode().value())
+                        .setMessage("this is a test error")
+                        .addDetails(Any.pack(errorInfo))
+                        .build());
         responseObserver.onError(t);
     }
 }
--- integration-tests/grpc-exceptions/src/test/java/com/example/grpc/exc/LegacyHelloGrpcServiceTestBase.java
+++ integration-tests/grpc-exceptions/src/test/java/com/example/grpc/exc/LegacyHelloGrpcServiceTestBase.java
@@ -3,8 +3,10 @@ package com.example.grpc.exc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import io.grpc.Metadata;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.quarkus.grpc.GrpcClient;
@@ -20,5 +22,16 @@ class LegacyHelloGrpcServiceTestBase {
         final StatusRuntimeException exception = assertThrows(StatusRuntimeException.class,
                 () -> stub.legacySayHello(HelloRequest.newBuilder().setName("Neo").build()));
         assertEquals(Status.Code.INVALID_ARGUMENT, exception.getStatus().getCode());
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    void legacySayHelloRuntime() {
+        final StatusRuntimeException exception = assertThrows(StatusRuntimeException.class,
+                () -> stub.legacySayHelloRuntime(HelloRequest.newBuilder().setName("Neo").build()));
+        assertEquals(Status.Code.INVALID_ARGUMENT, exception.getStatus().getCode());
+        Metadata trailers = exception.getTrailers();
+        Assertions.assertNotNull(trailers);
+        System.out.println("trailers = " + trailers);
     }
 }
